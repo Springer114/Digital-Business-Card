f22e4908cbf8882cb410a801deaff3f625177bef
Zackary Springer <springerzack04@yahoo.com>
2023-03-13 15:46:59 -0700

    added component files to application

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './components/Header'
+import Buttons from './components/Buttons'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   	return (
 		<div className="App">
-			
+			<Header />
+			<Buttons />
+			<Main />
+			<Footer />
 		</div>
 	);
 }
