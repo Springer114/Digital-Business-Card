c957c409d3371b3b538035c5f0de4c491c872333
Zackary Springer <springerzack04@yahoo.com>
2023-03-14 08:34:14 -0700

    added styles to header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,10 @@
 export default function Header() {
     return (
-        <div className='Header'>
-            <img src='../public/images/LauraSmith.png' alt='Laura Smith Head Shot' />
-            <h3>Laura Smith</h3>
-            <h4>Frontend Developer</h4>
-            <p>laurasmith.website</p>
+        <div className='header'>
+            <img src='/LauraSmith.png' alt='Laura Smith Head Shot' />
+            <p className="name">Laura Smith</p>
+            <p className="occupation">Frontend Developer</p>
+            <p className="email">laurasmith.website</p>
         </div>
     )
 }
