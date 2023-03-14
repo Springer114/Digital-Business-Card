5584094c9a793c450576f3148e06d4740503533f
Zackary Springer <springerzack04@yahoo.com>
2023-03-13 21:26:45 -0700

    started header component content, added image and images folder

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className='Header'>
+            <img src='../public/images/LauraSmith.png' alt='Laura Smith Head Shot' />
+            <h3>Laura Smith</h3>
+            <h4>Frontend Developer</h4>
+            <p>laurasmith.website</p>
+        </div>
+    )
+}
